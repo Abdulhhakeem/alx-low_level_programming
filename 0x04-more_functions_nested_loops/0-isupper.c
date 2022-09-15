069c3c39b90f7cc66775db5936cb522dc304b1d5
Abdulhhakeem <abdulhakeemopeade@gmail.com>
2022-09-15 04:51:07 -0700

    apple

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+
+  * _isupper - Check if a letter is upper
+
+  * @x: The number to be checked
+
+  *
+
+  * Return: 1 for upper letter or 0 for any else
+
+  */
+
+int_isupper(int x)
+
+{
+
+       if (x >= 65 && x <= 90)
+
+       {
+
+               return (1);
+
+       }
+
+  
+
+       return (0);
+
+root@15cda6016ac6:/alx-low_level_programming/0x04-more_functions_nested_loop# git add .
+
+wq
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My readme file for nested_loops task

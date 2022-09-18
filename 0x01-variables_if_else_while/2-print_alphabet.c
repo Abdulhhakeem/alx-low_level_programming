40f4e4c0772d5380d0ced98c8ee2e050fd1dac8d
Abdulhhakeem <abdulhakeemopeade@gmail.com>
2022-09-17 23:09:11 -0700

    2-print_alphabet.c
    \

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-/**
- * main - main block
- * Description: Use `putchar` function to print the alphabet in lowercase.
- *  Return: 0
- */
-int main(void
 
-{
-char c = 'a';
-while (c <= 'z')
-{
-putchar(c);
-c++;
-}
-putchar('\n');
-return (0);
-}
+
+#include <stdio.h>                                                                                                                                     
+/**                                                                                                                                                    
+*  main - printif the number is postive, zero or negative                                                                                              
+*                                                                                                                                                      
+* Description: using the main function                                                                                                                 
+* this program prints "Programming is postive, negative or zero                                                                                        
+* Return: 0                                                                                                                                            
+*/                                                                                                                                                     
+int main(void)                                                                                                                                         
+{         
+char ch;	
+for (ch = 'a' ; ch <= 'z' ; ch++)                                                                                                                      
+{      
+       putchar(ch);	
+}                                                                                                                                                      
+putchar('\n');                                                                                                                                         
+return (0);                                                                                                                                            
+}  

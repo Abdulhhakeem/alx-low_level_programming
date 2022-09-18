e4756ad6619822cc7643cec1d1db18407c381942
Abdulhhakeem <111154476+Abdulhhakeem@users.noreply.github.com>
2022-09-18 03:37:15 -1100

    pray

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
 
-
-#include <stdio.h>                                                                                                                                     
-/**                                                                                                                                                    
-*  main - printif the number is postive, zero or negative                                                                                              
-*                                                                                                                                                      
-* Description: using the main function                                                                                                                 
-* this program prints "Programming is postive, negative or zero                                                                                        
-* Return: 0                                                                                                                                            
-*/                                                                                                                                                     
-int main(void)                                                                                                                                         
-{         
-char ch;	
-for (ch = 'a' ; ch <= 'z' ; ch++)                                                                                                                      
-{      
-       putchar(ch);	
-}                                                                                                                                                      
-putchar('\n');                                                                                                                                         
-return (0);                                                                                                                                            
-}  
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
